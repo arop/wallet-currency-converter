6334d880030dd8f98d486308d722b94ac154477f
João Norim <norim_13@hotmail.com>
2016-11-30 22:02:08 +0000

    simple add/remove money page.

---
+++ MultiCurrencyWallet/MultiCurrencyWallet/MultiCurrencyWallet/InputValuePage.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MultiCurrencyWallet
+{
+    public class InputValuePage : ContentPage
+    {
+        Picker actionPicker;
+        Label entryLabel;
+        Entry valueEntry;
+        Button button;
+
+        public InputValuePage()
+        {
+
+            actionPicker = new Picker
+            {
+                Title = "Action",
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            actionPicker.Items.Add("Add");
+            actionPicker.Items.Add("Remove");
+            actionPicker.SelectedIndexChanged += actionChanged;
+
+            var entryLabel = new Label()
+            {
+                Text = "Amount:"
+            };
+            valueEntry = new Entry()
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Placeholder = "Amount",
+                Keyboard = Keyboard.Numeric
+            };
+
+            button = new Button()
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Text = "Add",
+                BackgroundColor = Utils.addColor
+            };
+            button.Clicked += OnButtonClicked;
+
+            var stack1 = new StackLayout()
+            {
+                Orientation = StackOrientation.Vertical,
+                Children = { actionPicker, entryLabel, valueEntry, button },
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            Content = new StackLayout{
+                Children = { stack1 }
+            };
+
+        }
+
+        private void actionChanged(object sender, EventArgs e)
+        {
+            //Debug.WriteLine("action changed");
+            if(((Picker)sender).SelectedIndex == 0)
+            {
+                button.Text = "Add";
+                button.BackgroundColor = Utils.addColor;
+            }
+            else
+            {
+                button.Text = "Remove";
+                button.BackgroundColor = Utils.removeColor;
+            }
+        }
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ MultiCurrencyWallet/MultiCurrencyWallet/MultiCurrencyWallet/Utils.cs
@@ -0,0 +1,10 @@
+﻿using Xamarin.Forms;
+
+namespace MultiCurrencyWallet
+{
+    class Utils
+    {
+        public static Color addColor = Color.FromRgb(70, 200, 70);
+        public static Color removeColor = Color.FromRgb(200, 70, 70);
+    }
+}
