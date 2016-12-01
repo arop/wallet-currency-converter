4bf805a1f94bac81d4578398a832195b83421c0e
João Norim <norim_13@hotmail.com>
2016-12-01 22:18:54 +0000

    display currency rate.

--- MultiCurrencyWallet/MultiCurrencyWallet/MultiCurrencyWallet/InputValuePage.cs
+++ MultiCurrencyWallet/MultiCurrencyWallet/MultiCurrencyWallet/InputValuePage.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Reflection.Emit;
 using System.Text;
 
@@ -11,15 +12,18 @@ namespace MultiCurrencyWallet
 {
     public class InputValuePage : ContentPage
     {
-        Picker actionPicker, currencyPicker;
-        Label entryLabel;
-        Entry valueEntry;
-        Button button;
-        DatabaseOps db;
+        public Picker actionPicker, currencyPicker;
+        public Label entryLabel, currencyRateLabel;
+        public Entry valueEntry;
+        public Button button;
+        public DatabaseOps db;
+        public Currency selectedCurrency;
 
         public InputValuePage(DatabaseOps db)
         {
             this.db = db;
+
+            ////////////////// ACTION PICKER //////////////////
             actionPicker = new Picker
             {
                 Title = "Action",
@@ -31,6 +35,7 @@ namespace MultiCurrencyWallet
             actionPicker.SelectedIndex = 0;
             actionPicker.SelectedIndexChanged += actionChanged;
 
+            ////////////////// VALUE INPUT //////////////////
             var entryLabel = new Label()
             {
                 Text = "Amount:"
@@ -42,7 +47,7 @@ namespace MultiCurrencyWallet
                 Keyboard = Keyboard.Numeric
             };
 
-
+            ////////////////// CURRENCY PICKER //////////////////
             currencyPicker = new Picker
             {
                 Title = "Currency",
@@ -52,10 +57,18 @@ namespace MultiCurrencyWallet
             
             foreach (Currency c in db.GetCurrencies())
             {
-                currencyPicker.Items.Add(c.code + " - " + c.rate);
+                currencyPicker.Items.Add(c.code);
             }
             currencyPicker.SelectedIndex = 0;
+            selectedCurrency = db.GetCurrencies().ElementAt(0);
+            currencyPicker.SelectedIndexChanged += currencyChanged;
 
+            ////////////////// CURRENT CURRENCY RATE //////////////////
+
+            currencyRateLabel = new Label();
+            updateCurrencyRateLabelText();
+
+            ////////////////// BUTTON //////////////////
             button = new Button()
             {
                 HorizontalOptions = LayoutOptions.FillAndExpand,
@@ -72,6 +85,7 @@ namespace MultiCurrencyWallet
                 HorizontalOptions = LayoutOptions.Center
             };*/
 
+            ////////////////// GRID //////////////////
             var grid = new Grid()
             {
                 HorizontalOptions = LayoutOptions.Center
@@ -89,12 +103,14 @@ namespace MultiCurrencyWallet
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            grid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
 
             grid.Children.Add(actionPicker, 1, 0);
             grid.Children.Add(entryLabel, 1, 1);
             grid.Children.Add(valueEntry, 1, 2);
             grid.Children.Add(currencyPicker, 1, 3);
-            grid.Children.Add(button, 1, 4);
+            grid.Children.Add(currencyRateLabel, 1, 4);
+            grid.Children.Add(button, 1, 5);
 
             Content = new StackLayout{
                 Children = { grid }
@@ -115,6 +131,20 @@ namespace MultiCurrencyWallet
                 button.Text = "Remove";
                 button.BackgroundColor = Utils.removeColor;
             }
+        }
+
+        private void currencyChanged(object sender, EventArgs e)
+        {
+            //Debug.WriteLine("action changed");
+            selectedCurrency = db.GetCurrencies().ElementAt(((Picker)sender).SelectedIndex);
+            updateCurrencyRateLabelText();
+        }
+
+        private void updateCurrencyRateLabelText()
+        {
+            this.currencyRateLabel.Text = string.Format("1 {0} = {1} {2}", Utils.favoriteCurrency.code,
+                    Math.Round(db.GetCurrency(selectedCurrency.code).getRate(Utils.favoriteCurrency), 5),
+                    selectedCurrency.code);
         }
 
         void OnButtonClicked(object sender, EventArgs e)
--- MultiCurrencyWallet/MultiCurrencyWallet/MultiCurrencyWallet/Utils.cs
+++ MultiCurrencyWallet/MultiCurrencyWallet/MultiCurrencyWallet/Utils.cs
@@ -6,5 +6,7 @@ namespace MultiCurrencyWallet
     {
         public static Color addColor = Color.FromRgb(70, 200, 70);
         public static Color removeColor = Color.FromRgb(200, 70, 70);
+
+        public static Currency favoriteCurrency; // load this when app loads
     }
 }
